d34c59ae7b4f8aa75b405896e4866c93b6c38f5c
Roy Hotrabhvanon <sukrith@gmail.com>
2013-02-15 12:29:42 +0000

    Working version, command line input only. Only has one working metastategy.

---
+++ src/rockpaperscissors/global.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rockpaperscissors;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Roy
+ */
+public class global {
+    
+   public static ArrayList list = new ArrayList();
+   
+   
+    
+}
